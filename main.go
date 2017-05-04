a44e14be94878c58ee6e561c946919a0c79456ee
Kumar Anirudha <anirudhastark@yahoo.com>
2017-05-05 02:30:25 +0530

    structure [wip]

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello world")
 	})
-
+	fmt.Println("Server started at localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
