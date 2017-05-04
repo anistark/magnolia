97f3a0a3d727d639aa03cabebdf6f7ffbc5f1205
Kumar Anirudha <anirudhastark@yahoo.com>
2017-05-05 02:47:42 +0530

    dockerfile fix

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 )
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello world")
-	})
-	fmt.Println("Server started at localhost:8080")
+	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080", nil)
 }
